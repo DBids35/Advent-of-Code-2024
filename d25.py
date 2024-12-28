69790730d6c1af581d30b47c6ad582df2e348585
Drew <andrew.bidlen@gmail.com>
2024-12-28 18:08:53 -0500

    keys and locks formatted

--- d25.py
+++ d25.py
@@ -11,24 +11,27 @@ def get_input() -> list[str]:
 def input_to_list_of_lists(input):
 	return [list(group) for key, group in groupby(input, key=lambda x: x != '') if key]
 
-def format_keys(keys:list[list[str]]) -> list[list[int]]:
-	pass
+def format_keys(keys:list[list[str]]) -> list[list[list[int]]]:
+	return [[row.count("#") for row in zip(*key[:-1][::-1])] for key in keys]
 
-def format_locks(locks:list[list[str]]) -> list[list[int]]:
-	pass
+def format_locks(locks:list[list[str]]) -> list[list[list[int]]]:
+	# top row will be full
+	# remove top row
+	# rotate 2d array
+	return [[row.count("#") for row in zip(*lock[1:][::-1])] for lock in locks]
 
-def input_to_keys_and_locks(input: list[str]) -> tuple[list[list[int]], list[list[int]]]:
-	list_of_lists = input_to_list_of_lists(input)
-	keys = [x for x in list_of_lists if x[0] == "....." and x[6] == "#####"]
-	locks = [x for x in list_of_lists if x not in keys]
+def input_to_keys_and_locks(input: list[list[str]]) -> tuple[list[list[list[int]]], list[list[list[int]]]]:
+	keys = [x for x in input if x[0] == "....." and x[6] == "#####"]
+	locks = [x for x in input if x not in keys]
 
 	return format_keys(keys), format_locks(locks)
 
 def main():
 	input = get_input()
 	input = input_to_list_of_lists(input)
-
-	return input
+	keys, locks = input_to_keys_and_locks(input)
+	print(locks)
+	return keys, locks
 
 if __name__ == "__main__":
-	print(main())
+	keys, locks = main()
