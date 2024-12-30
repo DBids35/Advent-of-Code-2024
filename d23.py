7b30b70b9481c751da93c2eb1aad654d926f9b64
Drew <andrew.bidlen@gmail.com>
2024-12-30 08:51:33 -0500

    d23 p2 networkx is cool

--- d23.py
+++ d23.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import networkx as nx
+
 
 def get_input():
 	with open("d23.txt", "r") as file:
@@ -24,10 +26,30 @@ def p1(input):
 				if a[0] == "t" or b[0]=="t" or c[0] == "t":
 					t_triangles += 1
 	return t_triangles
+
+def p2(input):
+
+	# 1) Create an undirected graph
+	G = nx.Graph()
+
+	# 2) Add edges from your pairs list
+	G.add_edges_from(input)
+
+	# 3) Use networkx to find all maximal cliques
+	all_maximal_cliques = list(nx.find_cliques(G))
+
+	# 4) Find the largest clique(s)
+	max_clique_size = max(len(clique) for clique in all_maximal_cliques)
+	largest_cliques = [clique for clique in all_maximal_cliques 
+					if len(clique) == max_clique_size]
+
+	print("Size of the largest clique:", max_clique_size)
+	print("All largest cliques:", sorted(largest_cliques))
+
 	
 
 
 if __name__ == "__main__":
 	input = get_input()
 
-	print(p1(input))
+	p2(input)
